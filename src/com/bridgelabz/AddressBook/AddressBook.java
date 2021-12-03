6843ecb6690278b29354a342e3ff8a4cafcdbccd
pknowledge <abhishekrawane1195@gmail.com>
2021-12-03 07:59:28 +0530

    Modified : Creating a new contact and adding it

--- src/com/bridgelabz/AddressBook/AddressBook.java
+++ src/com/bridgelabz/AddressBook/AddressBook.java
@@ -1,32 +1,31 @@
 package com.bridgelabz.AddressBook;
 
+import java.util.Scanner;
+
 public class AddressBook {
 
-	private final String firstName;
-	private final String lastName;
-	private final String address;
-	private final String cityName;
-	private final String stateName;
-	public final int zip;
-	public final long phoneNumber;
-	private final String emailID;
-
-	public AddressBook(String firstName, String lastName, String address, String cityName, String stateName, int zip,
-			long phoneNumber, String emailID) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-		this.cityName = cityName;
-		this.stateName = stateName;
-		this.zip = zip;
-		this.phoneNumber = phoneNumber;
-		this.emailID = emailID;
-	}
-
 	public static void main(String[] args) {
-		System.out.println("welcome to the address book program");
-		
-	}
+		Scanner sc = new Scanner(System.in);
+		Contact contact = new Contact();
+		System.out.println("How many Contact you want to add?");
+		int totalNumber = sc.nextInt();
 
+		int i = 1;
+		while (i <= totalNumber) {
+			System.out.println(i + "Address :");
+			contact.addContact();
+//			contact.display();
+			i++;
+		}
+		i = 1;
+		for (Contact s : contact.contactList) {
+
+			System.out.println("Contact.1 [ firstName=" + contact.getFirstName() + ", lastName=" + contact.getLastName()
+					+ ", address=" + contact.getAddress() + ", city=" + contact.getCity() + ", state="
+					+ contact.getState() + ", zip=" + contact.getZip() + ", phoneNumber=" + contact.getPhoneNumber()
+					+ ", email=" + contact.getEmail() + "]");
+			i++;
+		}
+		sc.close();
+	}
 }
